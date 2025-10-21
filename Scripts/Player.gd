6f37cd27626a10b38e5f5247438d008f69bb95a6
Ydris <ydris.benmansour@studioblackflag.com>
2025-10-21 16:27:43 +0200

    wip

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,18 +6,24 @@ class_name Player extends Node2D
 @export var raycast: RayCast2D
 
 @export var starting_speed: float = 150.0
-@export var shoot_dist: float = 100.0
+@export var shoot_dist: float = 150.0
+@export var retract_speed: float = 100.0
+@export var min_dist_on_grab: float = 20.0
+@export var gainable_speed_per_grab: float = 50.0
 
 var screen_size: Vector2
 var speed: Vector2
 var last_pos: Vector2
 var actual_speed: float
 var radius: float
+var rotation_angle: float = 0.0
+var first_dist_to_planet: float
+var dist_to_planet: float
+var min_grab_speed: float
+var max_grab_speed: float
 
 var grabbed_planet: Planet = null
 var aim_vec: Vector2 = Vector2.ZERO
-var rotation_angle: float = 0.0
-var dist_to_planet: float = 0.0
 var is_free: bool = true
 var can_shoot: bool = true
 var is_clockwise_rot: bool = true
@@ -27,6 +33,7 @@ func _ready() -> void:
 	actual_speed = starting_speed
 	speed = Vector2.RIGHT.rotated(randf() * TAU) * actual_speed
 	screen_size = get_viewport_rect().size
+	raycast.target_position = Vector2(shoot_dist, 0.0)
 	if collision_shape.shape is CircleShape2D:
 		radius = collision_shape.shape.radius
 
@@ -40,12 +47,13 @@ func _physics_process(delta: float) -> void:
 		free_move(delta)
 		rotate_viewfinder()
 	else:
+		handle_grabbed_distance(delta)
 		rotate_arround_grabbed_planet(delta)
 	rectify_position()
 	last_pos = global_position
 
 
-func get_inputs():
+func get_inputs() -> void:
 	aim_vec = Input.get_vector("J1_Aim_Left","J1_Aim_Right","J1_Aim_Up","J1_Aim_Down")
 	if aim_vec.length() <= 0.2:
 		aim_vec = Vector2.ZERO
@@ -66,12 +74,23 @@ func rotate_viewfinder() -> void:
 
 
 func rotate_arround_grabbed_planet(delta: float) -> void:
-	var dist: float = (grabbed_planet.global_position - global_position).length()
 	var dir: float = -1.0 if is_clockwise_rot else 1.0
-	var omega: float = (actual_speed / dist) * dir
+	var omega: float = (actual_speed / dist_to_planet) * dir
 	rotation_angle += omega * delta
-	global_position = grabbed_planet.global_position + Vector2.RIGHT.rotated(rotation_angle) * dist
+	global_position = grabbed_planet.global_position + Vector2.RIGHT.rotated(rotation_angle) * dist_to_planet
 	grapple.points[1] = grabbed_planet.global_position - global_position
+
+
+func handle_grabbed_distance(delta: float) -> void:
+	if aim_vec.length() > 0.2:
+		dist_to_planet = (grabbed_planet.global_position - global_position).length()
+		var strength = aim_vec.dot((grabbed_planet.global_position - global_position).normalized())
+		dist_to_planet -= strength * delta * retract_speed
+		if dist_to_planet <= grabbed_planet.radius + radius + min_dist_on_grab:
+			dist_to_planet = grabbed_planet.radius + radius + min_dist_on_grab
+		if dist_to_planet >= shoot_dist:
+			dist_to_planet = shoot_dist
+		actual_speed = min_grab_speed + ((max_grab_speed - min_dist_on_grab) * (1.0 - (dist_to_planet / shoot_dist)))
 
 
 func rectify_position() -> void:
@@ -109,12 +128,21 @@ func set_grapple_target(aim_dir: Vector2) -> Vector2:
 	var target: Vector2 = grapple.points[0] + aim_dir * shoot_dist
 	if raycast.is_colliding():
 		grabbed_planet = raycast.get_collider().get_parent() as Planet
-		var dist: Vector2 = global_position - grabbed_planet.global_position
-		target = -dist
-		rotation_angle = dist.angle()
-		is_clockwise_rot = true if dist.cross(speed) < 0 else false
+		var planet_to_player: Vector2 = global_position - grabbed_planet.global_position
+		target = -planet_to_player
+		set_grab_context(planet_to_player)
 		is_free = false
 	return target
+
+
+func set_grab_context(planet_to_player: Vector2) -> void:
+	rotation_angle = planet_to_player.angle()
+	is_clockwise_rot = true if planet_to_player.cross(speed) < 0 else false
+	first_dist_to_planet = planet_to_player.length()
+	dist_to_planet = first_dist_to_planet
+	var ratio = dist_to_planet / shoot_dist
+	min_grab_speed = actual_speed - ((1.0 - ratio) * gainable_speed_per_grab)
+	max_grab_speed = actual_speed + (ratio * gainable_speed_per_grab)
 
 
 func reset_grapple() -> void:
