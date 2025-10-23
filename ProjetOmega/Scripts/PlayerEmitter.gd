0de56ffb92a044130a2eb79f35e163600ebd170e
Ydris <ydris.benmansour@studioblackflag.com>
2025-10-23 18:12:47 +0200

    repair-sound

--- ProjetOmega/Scripts/PlayerEmitter.gd
+++ ProjetOmega/Scripts/PlayerEmitter.gd
@@ -1,8 +1,10 @@
 class_name PlayerEmitter extends Node2D
 
 @export var is_player1: bool
+
 var clock: Clock
 var player_to_follow: Player
+
 
 func _ready() -> void:
 	clock = get_parent() as Clock
@@ -10,6 +12,7 @@ func _ready() -> void:
 		player_to_follow = clock.player1
 	else:
 		player_to_follow = clock.player2
+
 
 func _physics_process(_delta: float) -> void:
 	global_position = player_to_follow.global_position
